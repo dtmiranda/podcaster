477641877055049282f0da11b79804020c5a0fab
Samuel Miranda <hortadito2@gmail.com>
2021-05-10 13:25:06 -0100

    create episode detail page and link/route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@
 
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { api } from '../services/api';
@@ -48,7 +49,9 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                 />
                 
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -88,7 +91,9 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                     />
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                   </td>
                   <td>{episode.members}</td>
                   <td style={{width:100}}>{episode.publishedAt}</td>
